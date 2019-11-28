a93648a057004b407ab5b06d33d9f234238a01b6
Flaringapp <ashpek01@gmail.com>
2019-11-28 11:36:37 +0200

    Refactor last kotlin classes to java

---
+++ app/src/main/java/com/flaringapp/base/app/Constants.java
@@ -0,0 +1,7 @@
+package com.flaringapp.base.app;
+
+public final class Constants {
+
+    public static final int ANIM_DURATION = 250;
+    
+}
--- app/src/main/java/com/flaringapp/base/app/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.flaringapp.base.app
-
-object Constants {
-
-    const val ANIM_DURATION = 250L
-
-}
