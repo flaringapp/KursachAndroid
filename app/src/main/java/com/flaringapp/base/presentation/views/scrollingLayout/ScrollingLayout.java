b6bd42303cf61aaaf004cc76b195f0688bfbc61c
Mallivance <ashpek01@gmail.com>
2019-12-01 00:10:03 +0200

    Fix tree screen UI

--- app/src/main/java/com/flaringapp/base/presentation/views/scrollingLayout/ScrollingLayout.java
+++ app/src/main/java/com/flaringapp/base/presentation/views/scrollingLayout/ScrollingLayout.java
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 public class ScrollingLayout extends RelativeLayout {
 
     private float mPosX = 0, mPosY = 0;
+
+    private View child;
 
     public ScrollingLayout(Context context) {
         super(context);
@@ -22,25 +25,54 @@ public class ScrollingLayout extends RelativeLayout {
         super(context, attrs, defStyle);
     }
 
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        child = getChildAt(0);
+    }
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        final int action = event.getAction();
-
-        switch (action) {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
-                mPosX = getX() - event.getRawX();
-                mPosY = getY() - event.getRawY();
+                mPosX = child.getX() - event.getRawX();
+                mPosY = child.getY() - event.getRawY();
                 break;
             }
 
             case MotionEvent.ACTION_MOVE: {
-                setX(event.getRawX() + mPosX);
-                setY(event.getRawY() + mPosY);
-                invalidate();
+                float newX = event.getRawX() + mPosX;
+                float newY = event.getRawY() + mPosY;
+
+                if (newX < 0) {
+                    child.setX(0);
+                } else if (newX > this.getWidth() - childWidth()) {
+                    child.setX(this.getWidth() - childWidth());
+                } else {
+                    child.setX(newX);
+                }
+
+                if (newY < 0) {
+                    child.setY(0);
+                } else if (newY > this.getHeight() - childHeight()) {
+                    child.setY(this.getHeight() - childHeight());
+                } else {
+                    child.setY(newY);
+                }
+
+                child.invalidate();
                 break;
             }
         }
         return true;
+    }
+
+    private float childWidth() {
+        return child.getWidth() * child.getScaleX();
+    }
+
+    private float childHeight() {
+        return child.getHeight() * child.getScaleY();
     }
 }
